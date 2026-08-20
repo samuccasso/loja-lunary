3073617f6b03cf7cc1bb14b836bc7bc9b7730754
Samuel Ramos Carvalho <samuelramoscarvalhoo@gmail.com>
2026-08-20 09:34:16 -0300

    Add files via upload

---
+++ Lunary-Store-main/js/carrinho.js
@@ -0,0 +1,121 @@
+// Pega o carrinho salvo
+let carrinho = JSON.parse(localStorage.getItem("carrinho")) || [];
+
+// Adicionar produto
+function adicionarCarrinho(nome, preco, imagem) {
+
+    carrinho.push({
+        nome,
+        preco,
+        imagem
+    });
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+    atualizarContador();
+
+    mostrarToast();
+
+}
+
+function atualizarContador() {
+
+    const contador = document.getElementById("contadorCarrinho");
+
+    if (!contador) return;
+
+    if (carrinho.length == 0) {
+
+        contador.style.display = "none";
+
+    } else {
+
+        contador.style.display = "flex";
+        contador.textContent = carrinho.length;
+
+    }
+
+}
+
+function mostrarCarrinho() {
+
+    const lista = document.getElementById("listaCarrinho");
+    const total = document.getElementById("total");
+    const quantidade = document.getElementById("quantidade");
+
+    if (!lista) return;
+
+    lista.innerHTML = "";
+
+    let valorTotal = 0;
+
+    carrinho.forEach((produto, index) => {
+
+        valorTotal += produto.preco;
+
+        lista.innerHTML += `
+
+        <div class="item">
+
+            <img src="${produto.imagem}">
+
+            <div class="item-info">
+
+                <h3>${produto.nome}</h3>
+
+                <p>R$ ${produto.preco.toFixed(2)}</p>
+
+            </div>
+
+            <button class="remover"
+            onclick="removerProduto(${index})">
+
+            Remover
+
+            </button>
+
+        </div>
+
+        `;
+
+    });
+
+    quantidade.innerHTML = carrinho.length;
+
+    total.innerHTML = "R$ " + valorTotal.toFixed(2);
+
+}
+
+function removerProduto(indice) {
+
+    carrinho.splice(indice, 1);
+
+    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+
+    mostrarCarrinho();
+
+}
+
+function mostrarToast() {
+
+    const toast = document.getElementById("toast");
+
+    toast.classList.add("mostrar");
+
+    setTimeout(() => {
+
+        toast.classList.remove("mostrar");
+
+    }, 3000);
+
+}
+
+fetch("../php/finalizar_compra.php", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(carrinho)
+});
+
+atualizarContador();
